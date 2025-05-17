26218338cb57c0afcb8b7954d73795f7afad50fd
Shivram Meena <75777650+shivrammeena@users.noreply.github.com>
2025-05-17 15:42:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const registerlink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
+const button =document.getElementById('toggle-btn');
+const body =document.body;
+
 registerlink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
 });
@@ -19,3 +22,15 @@ btnPopup.addEventListener('click', ()=>{
 iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
 });
+
+button.addEventListener ('click', () => {
+    if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
+        body.classList.add ('light-mode');
+        button.textContent = 'Dark Mode';
+    } else {
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+        button.textContent = 'Light Mode';
+    }
+})
